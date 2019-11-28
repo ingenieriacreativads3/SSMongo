3f7dde40c224b34ad140b9bffe5e9cf12dc26381
ingenieriacreativads3 <ingenieriacreativads3@gmail.com>
2019-11-28 15:06:53 -0300

    refactor

--- db-front/src/containers/SolicitudDeValidacion.js
+++ db-front/src/containers/SolicitudDeValidacion.js
@@ -92,24 +92,7 @@ class SolicitudDeValidacion extends React.Component {
                 <UserPanel></UserPanel>
 
             {/* <!-- Sidebar user panel (optional) --> */}
-<<<<<<< HEAD
                 <UserPanel></UserPanel>
-=======
-            {/*<!--
-            <div class="user-panel">
-                <div class="pull-left image">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image" />
-                </div>
-                <div class="pull-left info">
-                <p>Alexander Pierce</p>
-                
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-                </div>
-            </div>
-             --> */ }
-
-
->>>>>>> b7417c7882fdff4a790cf1a5f0cf3ede89028016
             {/* <!-- search form (Optional) --> */}
                 <SearchForm></SearchForm>
             {/* <!-- /.search form --> */}
