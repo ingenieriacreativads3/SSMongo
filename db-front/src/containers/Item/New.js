e5523aaf0d9686baa29d36b13a281ac08789f15e
ingenieriacreativads3 <ingenieriacreativads3@gmail.com>
2019-12-01 18:50:02 -0300

    Lista la vista

--- db-front/src/containers/Item/New.js
+++ db-front/src/containers/Item/New.js
@@ -3,7 +3,12 @@ import { Link } from "react-router-dom";
 import Header from "../../components/Header";
 import UserPanel from "../../components/UserPanel";
 import SearchForm from "../../components/SearchForm";
-import TablaSolicitudValidacion from '../../components/TablaSolicitudValidacion';
+
+import '../bower_components/bootstrap/dist/css/bootstrap.min.css'
+import '../bower_components/font-awesome/css/font-awesome.min.css'
+import '../bower_components/Ionicons/css/ionicons.min.css'
+import '../dist/css/AdminLTE.min.css'
+import '../dist/css/skins/_all-skins.min.css'
 
 class New extends React.Component {
 
@@ -17,193 +22,166 @@ class New extends React.Component {
 
   render(){
 
-    var SolicitudDeValidacion = [];
-
-    if (this.getSolicitudDeValidacion() === undefined) {
-
-      SolicitudDeValidacion = [];
-      
-    } else {
-
     return(
             
-      <div class="wrapper">
+      <div className="wrapper">
 
         <Header></Header>
 
-        <aside class="main-sidebar">
+        <aside className="main-sidebar">
+          <section className="sidebar">
 
-            <section class="sidebar">
+            <UserPanel></UserPanel>
+            <SearchForm></SearchForm>
 
-                <UserPanel></UserPanel>
-                <SearchForm></SearchForm>
+            <ul className="sidebar-menu" data-widget="tree">
+                  <li className="header">HEADER</li>
 
-                <ul class="sidebar-menu" data-widget="tree">
-                    <li class="header">HEADER</li>
-
-                    <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
-                    <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
-                    <li class="treeview">
-                    <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
-                        <span class="pull-right-container">
-                            <i class="fa fa-angle-left pull-right"></i>
-                        </span>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a href="#">Link in level 2</a></li>
-                        <li><a href="#">Link in level 2</a></li>
-                    </ul>
-                    </li>
-                </ul>
-            </section>
+                  <li className="active"><a href="#"><i className="fa fa-link"></i> <span>Link</span></a></li>
+                  <li><a href="#"><i className="fa fa-link"></i> <span>Another Link</span></a></li>
+                  <li className="treeview">
+                  <a href="#"><i className="fa fa-link"></i> <span>Multilevel</span>
+                      <span className="pull-right-container">
+                          <i className="fa fa-angle-left pull-right"></i>
+                      </span>
+                  </a>
+                  <ul className="treeview-menu">
+                      <li><a href="#">Link in level 2</a></li>
+                      <li><a href="#">Link in level 2</a></li>
+                  </ul>
+                  </li>
+              </ul>
+          </section>
         </aside>
 
-        <div class="content-wrapper">
+        <div className="content-wrapper">
 
-            <section class="content-header">
-            <h1>
-                Productos
-                <small>Optional description</small>
-            </h1>
-            <ol class="breadcrumb">
-                <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-                <li class="active">Here</li>
-            </ol>
+            <section className="content-header">
+              <h1>
+                  Item
+                  <small>Nuevo</small>
+              </h1>
+              <ol className="breadcrumb">
+                  <li><a href="#"><i className="fa fa-dashboard"></i>Empresa</a></li>
+                  <li><a href="#">Item</a></li>
+                  <li className="active">Nuevo</li>
+              </ol>
             </section>
 
-            {/* <!-- Content Header (Page header) --> */}
-            <section class="content-header">
-            <h1>
-                Data Tables
-                <small>advanced tables</small>
-            </h1>
-            <ol class="breadcrumb">
-                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                <li><a href="#">Tables</a></li>
-                <li class="active">Data tables</li>
-            </ol>
-            </section>
+            <section className="content container-fluid">
 
-            {/* <!-- Main content --> */}
-            <section class="content container-fluid">
+              <section className="content">
+                <div className="row">
+                  <div className="col-xs-6">
 
-            {/* <!--------------------------
-                | Your Page Content Here |
-                --------------------------> */}
-
-
-
-
-            {/* <!-- Main content --> */}
-            <section class="content">
-                <div class="row">
-                    <div class="col-xs-12">
-
-
-                    <TablaSolicitudValidacion solicitudValidacion={ SolicitudDeValidacion }></TablaSolicitudValidacion>
-                    {/* <!-- /.box --> */}
-
-
+                  <div className="box box-primary">
+                    <div className="box-header with-border">
+                      <h3 className="box-title">Nuevo Item</h3>
                     </div>
-                    {/* <!-- /.col --> */}
+                    <form role="form">
+                      <div className="box-body">
+
+                        <div className="form-group">
+                          <label for="nombreItem">Nombre</label>
+                          <input type="text" className="form-control" id="nombreItem" placeholder="Nombre"></input>
+                        </div>
+
+                        <div className="form-group">
+                          <label for="precioItem">Nombre</label>
+                          <input type="number" className="form-control" id="precioItem" placeholder="Precio"></input>
+                        </div>
+
+                        <div className="form-group">
+                          <label for="fotoItem">Foto</label>
+                          <input type="file" id="fotoItem"></input>
+                          <p className="help-block" >Elegir una imagen para este nuevo item</p>
+                        </div>
+
+                      </div>
+                      <div className="box-footer">
+                        <button type="submit" class="btn btn-primary">Nuevo Item</button>
+                      </div>
+                    </form>
+                  </div>
+
+                  </div>
                 </div>
-                {/* <!-- /.row --> */}
-            </section>
-            {/* <!-- /.content --> */}
-
-
+              </section>
 
             </section>
-            {/* <!-- /.content --> */}
         </div>
-        {/* <!-- /.content-wrapper --> */}
 
-        {/* <!-- Main Footer --> */}
-        <footer class="main-footer">
-            {/* <!-- To the right --> */}
-            <div class="pull-right hidden-xs">
+        <footer className="main-footer">
+          <div className="pull-right hidden-xs">
             Anything you want
-            </div>
-            {/* <!-- Default to the left --> */}
-            <strong>Copyright &copy; 2016 <a href="#">Company</a>.</strong> All rights reserved.
+          </div>
+          {/* <!-- Default to the left --> */}
+          <strong>Copyright &copy; 2016 <a href="#">Company</a>.</strong> All rights reserved.
         </footer>
 
-        {/* <!-- Control Sidebar --> */}
-        <aside class="control-sidebar control-sidebar-dark">
-            {/* <!-- Create the tabs --> */}
-            <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-            <li class="active"><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-            <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
-            </ul>
-            {/* <!-- Tab panes --> */}
-            <div class="tab-content">
-            {/* <!-- Home tab content --> */}
-            <div class="tab-pane active" id="control-sidebar-home-tab">
-                <h3 class="control-sidebar-heading">Recent Activity</h3>
-                <ul class="control-sidebar-menu">
+        <aside className="control-sidebar control-sidebar-dark">
+          <ul className="nav nav-tabs nav-justified control-sidebar-tabs">
+            <li className="active"><a href="#control-sidebar-home-tab" data-toggle="tab"><i className="fa fa-home"></i></a></li>
+            <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i className="fa fa-gears"></i></a></li>
+          </ul>
+          <div className="tab-content">
+            <div className="tab-pane active" id="control-sidebar-home-tab">
+              <h3 className="control-sidebar-heading">Recent Activity</h3>
+              <ul className="control-sidebar-menu">
                 <li>
-                    <a href="javascript:;">
-                    <i class="menu-icon fa fa-birthday-cake bg-red"></i>
-
-                    <div class="menu-info">
-                        <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
-
+                  <a href="javascript:;">
+                    <i className="menu-icon fa fa-birthday-cake bg-red"></i>
+                    <div className="menu-info">
+                        <h4 className="control-sidebar-subheading">Langdon's Birthday</h4>
                         <p>Will be 23 on April 24th</p>
                     </div>
-                    </a>
+                  </a>
                 </li>
-                </ul>
-                {/* <!-- /.control-sidebar-menu --> */}
+              </ul>
 
-                <h3 class="control-sidebar-heading">Tasks Progress</h3>
-                <ul class="control-sidebar-menu">
+              <h3 className="control-sidebar-heading">Tasks Progress</h3>
+              <ul className="control-sidebar-menu">
                 <li>
                     <a href="javascript:;">
-                    <h4 class="control-sidebar-subheading">
-                        Custom Template Design
-                        <span class="pull-right-container">
-                            <span class="label label-danger pull-right">70%</span>
-                        </span>
-                    </h4>
+                      <h4 className="control-sidebar-subheading">
+                          Custom Template Design
+                          <span className="pull-right-container">
+                            <span className="label label-danger pull-right">70%</span>
+                          </span>
+                      </h4>
 
-                    <div class="progress progress-xxs">
-                        <div class="progress-bar progress-bar-danger" style={{ "width": "70%" }}></div>
+                      <div className="progress progress-xxs">
+                        <div className="progress-bar progress-bar-danger" style={{ "width": "70%" }}></div>
                     </div>
                     </a>
                 </li>
-                </ul>
-                {/* <!-- /.control-sidebar-menu --> */}
+              </ul>
 
             </div>
-            {/* <!-- /.tab-pane --> */}
-            {/* <!-- Stats tab content --> */}
-            <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
-            {/* <!-- /.tab-pane --> */}
-            {/* <!-- Settings tab content --> */}
-            <div class="tab-pane" id="control-sidebar-settings-tab">
+          
+            <div className="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
+            <div className="tab-pane" id="control-sidebar-settings-tab">
                 <form method="post">
-                <h3 class="control-sidebar-heading">General Settings</h3>
+                  <h3 className="control-sidebar-heading">General Settings</h3>
 
-                <div class="form-group">
-                    <label class="control-sidebar-subheading">
-                    Report panel usage
-                    <input type="checkbox" class="pull-right" checked />
-                    </label>
+                  <div className="form-group">
+                      <label className="control-sidebar-subheading">
+                        Report panel usage
+                        <input type="checkbox" className="pull-right" checked />
+                      </label>
 
-                    <p>
-                    Some information about this general settings option
-                    </p>
-                </div>
-                {/* <!-- /.form-group --> */}
+                      <p>
+                        Some information about this general settings option
+                      </p>
+                  </div>
                 </form>
             </div>
-            {/* <!-- /.tab-pane --> */}
-            </div>
+          </div>
         </aside>
         {/* <!-- /.control-sidebar --> */}
         {/* <!-- Add the sidebar's background. This div must be placed
         immediately after the control sidebar --> */}
-        <div class="control-sidebar-bg"></div>
+        <div className="control-sidebar-bg"></div>
       </div>
 
     );
--- db-front/src/containers/SolicitudDeValidacion.js
+++ db-front/src/containers/SolicitudDeValidacion.js
@@ -104,25 +104,16 @@ class SolicitudDeValidacion extends React.Component {
             </section>
         </aside>
         <div class="content-wrapper">
+            
             <section class="content-header">
             <h1>
-                Page Header
-                <small>Optional description</small>
+                Solicitud de Validación
+                <small>Lista</small>
             </h1>
             <ol class="breadcrumb">
-                <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-                <li class="active">Here</li>
-            </ol>
-            </section>
-            <section class="content-header">
-            <h1>
-                Data Tables
-                <small>advanced tables</small>
-            </h1>
-            <ol class="breadcrumb">
-                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                <li><a href="#">Tables</a></li>
-                <li class="active">Data tables</li>
+                <li><a href="#"><i class="fa fa-dashboard"></i> Admin</a></li>
+                <li><a href="#">SolicitudesDeValidacion</a></li>
+                <li class="active">Lista</li>
             </ol>
             </section>
             <section class="content container-fluid">
