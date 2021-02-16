e2fd61f8668f5a5ab938f8f3da1ff1d2627f719f
yoha scarafia <yscarafia@e-partners.com.ar>
2021-02-16 17:33:15 -0300

    icono nombre usuario logueado, footer

--- app/src/components/Footer/Footer.tsx
+++ app/src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import EqualizerIcon from '@material-ui/icons/Equalizer';
 
 
-import {Grid, Typography, Avatar} from '@material-ui/core';
+import {Grid, Typography, Avatar, Container, Link,  TextField} from '@material-ui/core';
 
 
 import { connect } from 'react-redux'
@@ -96,6 +96,60 @@ class Footer extends React.Component {
           </Grid>
           </Grid>
     </footer>
+
+    //   <Typography component="footer" className={classes.root}>
+    //   <Container className={classes.container}>
+    //     <Grid container spacing={5}>
+    //       <Grid item xs={12} sm={6} md={12}>
+    //         <Grid
+    //           container
+    //           direction="column"
+    //           justify="flex-end"
+    //           spacing={2}
+    //         >
+    //           <Grid item >
+    //           <Typography variant="h5" align='left' gutterBottom>
+    //             Sobre Nosotros
+    //             </Typography>
+
+    //               <Typography variant="h6" align='left' gutterBottom>
+    //                 En esta plataforma podras encontrar de manera rápida y sencilla a tus proveedores. 
+    //               Registrate, Ingresá y comenzá a hacer tus pedidos. 
+    //               </Typography>
+    //           </Grid>
+    //           <Grid item>
+    //           <Avatar  src={logo} className={classes.avatar} ></Avatar>
+    //           </Grid>
+    //         </Grid>
+    //       </Grid>
+          
+    //       <Grid item xs={12} sm={6} md={12}>
+    //         <Typography variant="h6"  gutterBottom>
+    //         <Typography variant="h5" align='left' gutterBottom>
+    //                 ¿Por qué elegirnos?
+    //               </Typography>
+
+    //                 <Typography variant="h6" align='left'  gutterBottom>
+    //                   <VerifiedUserIcon fontSize="large" style={{ color: '#d93211' }} />
+    //                   Todas las empresas de esta plataforma, son legalmente verificadas
+    //                 </Typography>
+
+    //                 <Typography variant="h6" align='left' gutterBottom>
+    //                   <ShoppingCartIcon fontSize="large" style={{ color: '#d93211' }} />
+    //                   Amplia oferta de productos y servicios
+    //                 </Typography>
+
+    //                 <Typography variant="h6" align='left' gutterBottom>
+    //                 <EqualizerIcon fontSize="large" style={{ color: '#d93211' }} />
+    //                 Perfil personalizado, con estadísticas acerca de tus negocios en la plataforma
+    //                 </Typography>
+    //         </Typography>
+           
+    //       </Grid>
+          
+    //     </Grid>
+    //   </Container>
+    // </Typography>
      
 
     );
